9d660f891a223a2ba5366b8aedae6ca04a815442
Amir_Ghaderian <it.ghaderian@gmail.com>
2024-10-01 17:32:47 +0330

    some modern style

--- src/Components/header/Header.tsx
+++ src/Components/header/Header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Note from "../../types/types";
+import './Header.css'; // فرمت CSS را به اینجا اضافه کنید
 
 // تعریف نوع پروپس‌ها
 interface NoteHeaderProps {
@@ -11,13 +12,12 @@ interface NoteHeaderProps {
 const NoteHeader: React.FC<NoteHeaderProps> = ({ notes, sortBy, onSort }) => {
   return (
     <div className="note-header">
-      <h1>my notes ({notes.length})</h1>
-      <select value={sortBy} onChange={onSort}>
-      <option value="no-sort">no Sorting</option>
-
-        <option value="latest">Sort based on latest notes</option>
-        <option value="earliest">Sort based on earliest notes</option>
-        <option value="completed">Sort based on completed notes</option>
+      <h1>My Notes ({notes.length})</h1>
+      <select value={sortBy} onChange={onSort} className="note-sort-select">
+        <option value="no-sort">No Sorting</option>
+        <option value="latest">Sort by Latest</option>
+        <option value="earliest">Sort by Earliest</option>
+        <option value="completed">Sort by Completed</option>
       </select>
     </div>
   );
